914f70c2cbf976f4b0eaf950439d8b849b256bed
harshit_garg <harshitgarg3008@gmail.com>
2025-05-03 22:10:49 +0530

    datatype

---
+++ 01_basics/01_var.js
@@ -0,0 +1,12 @@
+const accountid = 144553
+let accountemail = "harshit@gmail.com"
+var accountpass="12345"
+accountcity ="jaipur"
+let accountstate
+
+// accountid=2
+accountemail="hc.com"
+accountpass="212121"
+accountcity="delhi"
+console.log(accountid);
+console.table([accountemail,accountid,accountpass,accountcity,accountstate])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert(3+3)
+
+console.log(3+30)
+console.log("dfsa")
+
+
+
