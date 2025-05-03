a22238bce59eae8796cf6f981dc1186653d06bf0
harshit_garg <harshitgarg3008@gmail.com>
2025-05-03 22:21:16 +0530

    datatype

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -7,3 +7,19 @@ console.log("dfsa")
 
 
 
+let name="harsh"
+let age = 18
+let islogin= false
+let state;
+// bigint
+// null
+// undefined
+// symbol =unipue
+
+
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null); //object
