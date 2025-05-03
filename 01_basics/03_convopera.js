0e356cc5de7dbf2f25b9a004a2d934e658313bdb
harshit_garg <harshitgarg3008@gmail.com>
2025-05-03 23:02:46 +0530

    conv

--- 01_basics/03_convopera.js
+++ 01_basics/03_convopera.js
@@ -1,20 +1,39 @@
 let score ="gdf"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueinnum = Number(score)
 
-console.log(typeof valueinnum);
-console.log(typeof(valueinnum));
-console.log(valueinnum);
+// console.log(typeof valueinnum);
+// console.log(typeof(valueinnum));
+// console.log(valueinnum);
 let islogged= "sdv"
 let bolllogin = Boolean(islogged)
 
-console.log(bolllogin);
+// console.log(bolllogin);
 
 // 1=true 0=false ""=false "sdv"=true
 let somenm =33
 let stringnum = String(somenm)
-console.log(stringnum);
+// console.log(stringnum);
 
+
+let value =3
+let negvalue=-value
+// console.log(negvalue);
+
+let str1 ="hello"
+let str2 = "fbdsv"
+let str3 =str1+str2
+// console.log(str3);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+
+let gamecount=100
+console.log(++gamecount);
+console.log(gamecount++);
