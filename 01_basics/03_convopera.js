20e333c12862557c9143eb5493d26f221fcfcc3b
harshit_garg <harshitgarg3008@gmail.com>
2025-05-03 22:43:27 +0530

    conv

---
+++ 01_basics/03_convopera.js
@@ -0,0 +1,20 @@
+let score ="gdf"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnum = Number(score)
+
+console.log(typeof valueinnum);
+console.log(typeof(valueinnum));
+console.log(valueinnum);
+let islogged= "sdv"
+let bolllogin = Boolean(islogged)
+
+console.log(bolllogin);
+
+// 1=true 0=false ""=false "sdv"=true
+let somenm =33
+let stringnum = String(somenm)
+console.log(stringnum);
+
