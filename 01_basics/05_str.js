3cacf3049d85d16ece9cd80e6b235d3c2cdfef70
harshit_garg <harshitgarg3008@gmail.com>
2025-05-04 00:57:33 +0530

    string

---
+++ 01_basics/05_str.js
@@ -0,0 +1,35 @@
+const name ="Dbf"
+const repo=44
+// console.log(name +repo +"value");
+// console.log(`hello ${name} repo ${repo}`);
+const gamename = new String('Harsh')
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(4));
+// console.log(gamename.indexOf('a'));
+// console.log(gamename.charAt(4));
+const gamename1 = new String('Harsh-hc')
+const newstr=gamename1.substring(0,4)
+// console.log(newstr);
+// console.log(gamename1.length);
+const newst1=gamename1.slice(-7,4)
+// console.log(newst1);
+const newstr3="    fvrgdf   "
+console.log(newstr3);
+console.log(newstr3.trim());
+
+const url="https://rdtgfv.com/hnbgfhgb%20rhfngdbvt"
+console.log(url.replace('%20','-'))
+console.log(url.includes('https'))
+
+console.log(gamename1.split('-'));
+
+
+
+
+
+
+
+
