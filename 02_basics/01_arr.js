446e7bacafef0e8df3860b91054d749887e0f053
harshit_garg <harshitgarg3008@gmail.com>
2025-05-05 19:38:41 +0530

    arr

---
+++ 02_basics/01_arr.js
@@ -0,0 +1,43 @@
+const myarr =[0,1,2,3,4,5]
+const hero=["shakti","super"]
+
+
+
+
+const arr2= new Array(1,2,3,4)
+// console.log(myarr[1]);
+
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+// myarr.unshift(9)
+
+// myarr.shift()
+// console.log(myarr.includes(9));
+
+// console.log(myarr.indexOf(9));
+
+
+// console.log(myarr.indexOf(2));
+
+
+
+const arr1 = myarr.join()
+
+
+// console.log(myarr);
+// console.log(arr1);
+
+// console.log(typeof arr1);
+
+console.log("A ",myarr);
+
+const myn1=myarr.slice(1,3)
+console.log(myn1);
+
+console.log("B ",myarr);
+const myn2=myarr.splice(1,3)
+console.log("C ",myarr);
+
+console.log(myn2);
