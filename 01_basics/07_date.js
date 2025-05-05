5c552b12202e35e4574de6460ec989fcbcb72c42
harshit_garg <harshitgarg3008@gmail.com>
2025-05-05 19:11:34 +0530

    dateandtime

---
+++ 01_basics/07_date.js
@@ -0,0 +1,43 @@
+let mydate = new Date()
+// console.log(mydate);
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+// console.log(mydate.toLocaleDateString());
+// console.log(typeof mydate);
+
+let mynewdate = new Date(2023,0,23)
+// console.log(mynewdate);
+// console.log(mynewdate.toDateString());
+let mynew1date = new Date(2023,0,23,5,3)
+// console.log(mynew1date.toLocaleString());
+let mydate1 = new Date("2023-01-14")
+// console.log(mydate1.toLocaleString());
+
+// console.log(mydate1.toLocaleDateString());
+
+
+let mydate2 = new Date("01-14-2023")
+// console.log(mydate2.toLocaleDateString());
+// console.log(mydate2.toDateString());
+
+let mytimestamp =Date.now()
+// console.log(mytimestamp);
+// console.log(mydate2.getTime());
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+let date=new Date()
+// console.log(date.getMonth()+1);
+// console.log(date.getDay());
+// `${date.getDay()}` and time is 
+
+// console.log(date.toLocaleDateString('default',{
+//     weekday:"long",
+//     timeZone:'Asia/kolkata'
+// }));
+
+
+
